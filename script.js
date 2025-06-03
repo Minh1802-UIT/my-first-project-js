7a8097f6e8510e1e9d493f1a1ac6c2f125b48bb5
Minh1802-UIT <24210232@ms.uit.edu.vn>
2025-06-03 09:42:49 +0700

    Initial commit: Add HTML, CSS, and JS for click counter

---
+++ script.js
@@ -0,0 +1,12 @@
+﻿// Lấy các phần tử HTML
+const clickButton = document.getElementById('clickButton');
+const clickCountDisplay = document.getElementById('clickCount');
+
+// Khởi tạo biến đếm
+let count = 0;
+
+// Thêm sự kiện click cho nút
+clickButton.addEventListener('click', function() {
+    count++; // Tăng biến đếm
+    clickCountDisplay.textContent = count; // Cập nhật số lần click hiển thị trên trang
+});
